1384d0502a40d28178c19eee1335d64e68e3d80d
Frank Conlon <avanola@gmail.com>
2018-09-20 14:58:34 -0500

    added diagnostics template

--- diagnostics/views.py
+++ diagnostics/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h2>Under Construction</h2>')
-
-# Create your views here.
+    return render(request, "diagnostics/index.html", {})
